1706af35c75360f7e52346545f793f7e03c43f38
Punyo <49717092+Punyo@users.noreply.github.com>
2025-08-29 11:18:06 +0900

    feat: com.punyo.casherapp.data.transaction.*のDIの設定を追加

--- composeApp/src/commonMain/kotlin/com/punyo/casherapp/application/di/RepositoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/punyo/casherapp/application/di/RepositoryModule.kt
@@ -2,9 +2,12 @@ package com.punyo.casherapp.application.di
 
 import com.punyo.casherapp.data.product.ProductRepository
 import com.punyo.casherapp.data.product.ProductRepositoryImpl
+import com.punyo.casherapp.data.transaction.TransactionRepository
+import com.punyo.casherapp.data.transaction.TransactionRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule =
     module {
         single<ProductRepository> { ProductRepositoryImpl(get()) }
+        single<TransactionRepository> { TransactionRepositoryImpl(get()) }
     }
--- composeApp/src/commonMain/kotlin/com/punyo/casherapp/application/di/SourceModule.kt
+++ composeApp/src/commonMain/kotlin/com/punyo/casherapp/application/di/SourceModule.kt
@@ -1,9 +1,11 @@
 package com.punyo.casherapp.application.di
 
 import com.punyo.casherapp.data.product.source.ProductLocalDataSource
+import com.punyo.casherapp.data.transaction.source.TransactionLocalDataSource
 import org.koin.dsl.module
 
 val sourceModule =
     module {
         single { ProductLocalDataSource(get()) }
+        single { TransactionLocalDataSource(get()) }
     }
