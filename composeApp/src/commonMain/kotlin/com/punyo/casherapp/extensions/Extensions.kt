0b134707d3532ff684a327d431e497f8555f0c0a
Punyo <49717092+Punyo@users.noreply.github.com>
2025-08-30 11:27:03 +0900

    feat: add extension function to convert Instant to formatted date string

--- composeApp/src/commonMain/kotlin/com/punyo/casherapp/extensions/Extensions.kt
+++ composeApp/src/commonMain/kotlin/com/punyo/casherapp/extensions/Extensions.kt
@@ -1,6 +1,9 @@
 package com.punyo.casherapp.extensions
 
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 fun LocalDateTime.startOfDay(): LocalDateTime = LocalDateTime(
     year = year,
@@ -20,3 +23,12 @@ fun LocalDateTime.endOfDay(): LocalDateTime = LocalDateTime(
     second = 59,
     nanosecond = 999_999_999,
 )
+
+fun Instant.toDateString(timeZone: TimeZone = TimeZone.currentSystemDefault()): String {
+    val dateTime = this.toLocalDateTime(timeZone)
+    return "${dateTime.year}/${dateTime.monthNumber.toString().padStart(2, '0')}/${
+        dateTime.dayOfMonth.toString().padStart(2, '0')
+    } ${dateTime.hour.toString().padStart(2, '0')}:${
+        dateTime.minute.toString().padStart(2, '0')
+    }:${dateTime.second.toString().padStart(2, '0')}"
+}
